79db019c5a6ffc980dd2849823c4f0c28389819e
OC-ZavierHill26 <zavier.hill@student.onslow.school.nz>
2026-04-23 02:36:50 +0000

    Apr 23, 2026, 2:36 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,42 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-
-       //prints Onslow College.
-       //  print("Onslow College")
-
-/* Task 1
-My initals printed on the screen
-*/
-
-print("Z Z Z Z Z  H     H")
-print("      Z    H     H")
-print("    Z      H H H H")
-print("  Z        H     H")
-print("Z Z Z Z Z  H     H")
-
-// First constant firstName
-let firstName = "Zavier"
-print(firstName)
-
-@main
-struct SwiftPlayground {
-    static func main() {
         let adjective1 ="amazing"
         let noun1 = "chad"
         let verb1 = "dance"
 
-let madLibString = "Once upon a time, there was a \
-(adjective1) \(noun1) who loved to \(verb1) every day."
+let madLibString = "Once upon a time, there was a \(adjective1) \(noun1) who loved to \(verb1) every day."
 
 
 
     }
 }
-    }
-}
-
 
 
 
