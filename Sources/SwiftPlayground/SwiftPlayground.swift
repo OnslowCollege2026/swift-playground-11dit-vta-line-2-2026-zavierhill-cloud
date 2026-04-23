c8fefa1e69622306cd301afbcc86c00b5b1efe45
OC-ZavierHill26 <zavier.hill@student.onslow.school.nz>
2026-04-23 02:35:18 +0000

    Apr 23, 2026, 2:35 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -26,9 +26,9 @@ print(firstName)
 @main
 struct SwiftPlayground {
     static func main() {
-        let adjective1 ="beautiful"
-        let noun1 = "toleit paper"
-        let verb1 = "sing"
+        let adjective1 ="amazing"
+        let noun1 = "chad"
+        let verb1 = "dance"
 
 let madLibString = "Once upon a time, there was a \
 (adjective1) \(noun1) who loved to \(verb1) every day."
